f0e735fa94890e703122267133f9763a10fca399
Mdbshero <mdbshero@gmail.com>
2022-03-31 18:16:21 -0500

    Added middleware song validation. Checked through Postman.

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
 // Imports
 const express = require("express");
-const repoContext = require("./repository/repository-wrapper")
+const repoContext = require("./repository/repository-wrapper");
+const songValidate = require('./middleware/song-validation')
 const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // Endpoints
 // http://localhost:5005 (BASE URL)
 
 // GET songs
 app.get("/api/songs", (req, res) => {
-    const songs = repoContext.songs.findAllSongs();
-    return res.send(songs);
-})
+  const songs = repoContext.songs.findAllSongs();
+  return res.send(songs);
+});
 
 // GET songs by ID
-app.get("/api/songs/:id", (req,res) => {
-    const id= req.params.id;
-    const songs = repoContext.songs.findSongById(id);
-    return res.send(songs);
-})
+app.get("/api/songs/:id", (req, res) => {
+  const id = req.params.id;
+  const songs = repoContext.songs.findSongById(id);
+  return res.send(songs);
+});
 
 //POST songs
-app.post("/api/songs", (req,res) =>{
-    const newSong = req.body;
-    const addedSong = repoContext.songs.createSong(newSong);
-    return res.send(addedSong);
-})
+app.post("/api/songs", [songValidate], (req, res) => {
+  const newSong = req.body;
+  const addedSong = repoContext.songs.createSong(newSong);
+  return res.send(addedSong);
+});
 
 //Server Start
 const PORT = process.env.PORT || 5005;
 
 app.listen(PORT, () => {
-    console.log(`Server running! On PORT: ${PORT}`);
+  console.log(`Server running! On PORT: ${PORT}`);
 });
