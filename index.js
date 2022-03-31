7964bf4652c2530a2fd29c724f5865fed74405bc
Mdbshero <mdbshero@gmail.com>
2022-03-31 18:02:33 -0500

    Added GET Songs and GET songs by ID.

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 // Imports
 const express = require("express");
+const repoContext = require("./repository/repository-wrapper")
 const app = express();
 
 // Middleware
 app.use(express.json());
 
+// Endpoints
+// http://localhost:5005 (BASE URL)
+
+// GET songs
+app.get("/api/songs", (req, res) => {
+    const songs = repoContext.songs.findAllSongs();
+    return res.send(songs);
+})
+
+// GET songs by ID
+app.get("/api/songs/:id", (req,res) => {
+    const id= req.params.id;
+    const songs = repoContext.songs.findSongById(id);
+    return res.send(songs);
+})
 
 //Server Start
 const PORT = process.env.PORT || 5005;
